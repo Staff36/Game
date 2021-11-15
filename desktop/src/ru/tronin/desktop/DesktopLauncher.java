9b9b0f777d9a5bf820e1b167f41114d248c7ff41
Dmitry <dima-tronin@mail.ru>
2021-11-15 23:03:36 +0300

    ht 1 -Hometask have been finished

--- desktop/src/ru/tronin/desktop/DesktopLauncher.java
+++ desktop/src/ru/tronin/desktop/DesktopLauncher.java
@@ -2,11 +2,15 @@ package ru.tronin.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
 import ru.tronin.MyGame;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGame(), config);
-	}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 400;
+        config.height = 700;
+        config.resizable = false;
+        new LwjglApplication(new MyGame(), config);
+    }
 }
