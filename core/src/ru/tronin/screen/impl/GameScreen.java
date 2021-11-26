b5f77d844b0d3508a099beb5469faa1b3c8d621d
Dmitry <dima-tronin@mail.ru>
2021-11-26 23:07:09 +0300

    Hometask 4 have been finished

--- core/src/ru/tronin/screen/impl/GameScreen.java
+++ core/src/ru/tronin/screen/impl/GameScreen.java
@@ -1,23 +1,40 @@
 package ru.tronin.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.tronin.math.Rect;
 import ru.tronin.screen.BaseScreen;
 import ru.tronin.sprite.impl.Background;
+import ru.tronin.sprite.impl.Ship;
+import ru.tronin.sprite.impl.Star;
 
 
 public class GameScreen extends BaseScreen {
-
+    public final float V_LEN = 0.01f;
+    private static final int STAR_COUNT = 256;
     private Texture bg;
     private Background background;
+    private Star[] stars;
+    private TextureAtlas atlas;
+    private Ship ship;
+    private Texture shipTexture;
+    private Vector2 touchPos;
 
     @Override
     public void show() {
         super.show();
+        shipTexture = new Texture("textures/ship.png");
+        atlas = new TextureAtlas("textures/menuAtlas.tpack");
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+        ship = new Ship(shipTexture);
+        touchPos = new Vector2();
     }
 
     @Override
@@ -31,12 +48,18 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        ship.resize(worldBounds);
     }
 
     @Override
     public void dispose() {
         super.dispose();
         bg.dispose();
+        atlas.dispose();
+        shipTexture.dispose();
     }
 
     @Override
@@ -46,16 +69,24 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
+        touchPos.set(touch);
+        ship.setNewMovementVector(touch,V_LEN);
         return super.touchUp(touch, pointer, button);
     }
 
     private void update(float delta) {
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw() {
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        ship.draw(batch, touchPos);
         batch.end();
     }
 }
--- core/src/ru/tronin/sprite/impl/Ship.java
+++ core/src/ru/tronin/sprite/impl/Ship.java
@@ -11,31 +11,43 @@ import ru.tronin.sprite.Sprite;
 public class Ship extends Sprite {
 
     private Vector2 movement;
+    private float heightPos = 0;
+    private Rect worldBounds;
 
     public Ship(Texture texture) {
         super(new TextureRegion(texture));
         movement = new Vector2();
+        worldBounds = new Rect();
+
     }
 
     @Override
     public void resize(Rect worldBounds) {
         setHeightProportion(worldBounds.getHeight() * 0.2f);
         this.pos.set(worldBounds.pos);
-
+        heightPos = worldBounds.getBottom() + 0.15f ;
+        this.worldBounds = worldBounds;
     }
 
     public void setNewMovementVector(Vector2 touch, float vLen){
-        movement.set(touch.cpy().sub(pos).setLength(vLen));
+        Vector2 vector2 = touch.cpy();
+        vector2.y = heightPos;
+        vector2.sub(pos).setLength(vLen);
+        movement.set(vector2);
     }
-
-
 
     protected void move(Vector2 touchPos){
-        if (this.pos.dst(touchPos) > movement.len()){
+        pos.y = heightPos;
+        if (Math.abs(touchPos.x - pos.x) > movement.len()){
             this.pos.add(movement);
         }
+        if (this.getRight() >= worldBounds.getRight()){
+            pos.x = worldBounds.getRight() - this.getHalfWidth();
+        }
+        if (this.getLeft() <= worldBounds.getLeft()){
+            pos.x = worldBounds.getLeft() + this.getHalfWidth();
+        }
     }
-
 
     public void draw(SpriteBatch batch, Vector2 touchPos) {
         move(touchPos);
